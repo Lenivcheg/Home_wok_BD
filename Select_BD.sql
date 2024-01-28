6a623425c3feeae9449d8c84b260b05a6aedf476
Sergey Nogin <noginn@ya.ru>
2024-01-28 19:28:14 +0300

    second_commit

--- Select_BD.sql
+++ Select_BD.sql
@@ -28,21 +28,46 @@ where  name  like '%my%';
 
 -- Количество исполнителей в каждом жанре.
 
-
+select  name,  count(performer_id)  from ganere g
+left join ganere_performer gp on g.id = gp.ganere_id
+group by name;
 
 --Количество треков, вошедших в альбомы 2019–2020 годов.
+-- почему то не отображает корректно, вместо 6и треков 3 ???
 
+select count(name)  from song s
+full join album a on s.id = a.id
+where yer_of_release between 1995 and 2030
+;
 
+--Так тоже не сработало
 
+select title, yer_of_release, count(s.id)  from album a
+join song s on a.id = s.id
+where yer_of_release between 1995 and 2012
+group by a.title , a.yer_of_release
+;
 -- Средняя продолжительность треков по каждому альбому.
 
-
-
+select avg(duration), title  from song s
+join album a on s.id = a.id
+group by title
+;
 -- Все исполнители, которые не выпустили альбомы в 2020 году.
 
-
+select name from performer p
+join performer_album pa on p.id = pa.performer_id
+join album a on a.id = pa.performer_id
+where a.yer_of_release < 2000
+;
 -- Названия сборников, в которых присутствует конкретный исполнитель (выберите его сами).
 
-
-
+select c.title from collection c
+left join song_collection sc on c.id = sc.song_id
+left join song s on sc.song_id = s.id
+left join album a on s.album_id =  a.id
+left join performer_album pa on a.id = pa.album_id
+left join performer p on pa.performer_id = p.id
+--where p.name = "Ferry"
+;
 
